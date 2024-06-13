7653b208f5d6a0ace2d59ee7701a97f74f698000
liangrui <liangrui@yy.com>
2024-06-13 10:45:53 +0800

    client jar update

--- src/main/java/com/yy/bigdata/orc/OrcCoreTest.java
+++ src/main/java/com/yy/bigdata/orc/OrcCoreTest.java
@@ -2,6 +2,11 @@ package com.yy.bigdata.orc;
 
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * hadoop jar hdfs-client-op-1.0-SNAPSHOT.jar com.yy.bigdata.orc.OrcCoreTest '/hive_warehouse/abtest.db/yy_abtest_liucun_detail_server/dt=2022-03-22/part-65033-38a9fea1-be19-482d-845c-9e1aa8156af7.c000' '10.12.65.81,10.12.67.12'
+ *
+ * hadoop jar hdfs-client-op-1.0-SNAPSHOT.jar com.yy.bigdata.orc.OrcCoreTest '/hive_warehouse/abtest.db/yy_abtest_liucun_detail_server/dt=2022-03-22/part-65033-38a9fea1-be19-482d-845c-9e1aa8156af7.c000' '10.12.66.16,10.12.67.12'
+ */
 public class OrcCoreTest {
     public static void main(String[] args) {
         String filePath = "";
